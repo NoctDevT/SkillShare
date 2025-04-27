2cbd8a5a08f7a3411708dec227fb17e847af2569
NoctDevT <korzone0@gmail.com>
2025-04-27 09:46:49 -0600

    abstracting schemas, exporting infered types, removing ID from user

--- backend/src/models/user/UserSchema.ts
+++ backend/src/models/user/UserSchema.ts
@@ -3,8 +3,26 @@ import { z } from "zod";
 export const UserTypeEnum = z.enum(["STUDENT", "TEACHER", "BOTH"]);
 export const AccountStatusEnum = z.enum(["NEW", "PENDING", "VERIFIED"]);
 
+export const SkillSchema = z.object({
+  id: z.string().uuid().optional(), 
+  name: z.string(),
+  ownerId: z.string().uuid()
+});
+
+export const SessionSchema = z.object({
+  id: z.string().uuid().optional(), 
+  studentId: z.string().uuid(),
+  teacherId: z.string().uuid(),
+  topic: z.string(),
+  scheduledAt: z.coerce.date(), 
+});
+
+export const SkillsArraySchema = z.array(SkillSchema);
+export const SessionsArraySchema = z.array(SessionSchema);
+
+
 export const UserSchema = z.object({
-  id: z.string(),
+  // id: z.string(),
   name: z.string(),
   email: z.string().email(),
   type: UserTypeEnum,
@@ -14,3 +32,26 @@ export const UserSchema = z.object({
   providerId: z.string(),
   accountStatus: AccountStatusEnum
 });
+
+export const UserCreateSchema = z.object({
+  name: z.string().optional(),
+  email: z.string().email(),
+  provider: z.string().optional(),
+  providerId: z.string().optional(),
+  accountStatus: AccountStatusEnum,
+  type: UserTypeEnum,
+  skills: SkillsArraySchema.optional(),
+  studentSessions: SessionsArraySchema.optional(),
+  teacherSessions: SessionsArraySchema.optional(),
+})
+
+
+export const OnboardingSchema = z.object({
+  name: z.string(),
+  type: UserTypeEnum,
+  accountStatus: AccountStatusEnum
+});
+
+export type UserInputType = z.infer<typeof UserCreateSchema>;
+export type UserOnboardingSchema = z.infer<typeof OnboardingSchema>;
+
